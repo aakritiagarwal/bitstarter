764d5e1d47ebb0f7358a384bc31bf38492ebe54a
aakritiagarwal <agarwal.aakriti@gmail.com>
2013-07-07 16:23:11 +0000

    errors fixed

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //var buffer = new Buffer(28);
+  var fs = require('fs');
   var buffer  = fs.readFileSync('index.html');
   response.send(buffer.toString("utf-8"));
   //response.send('Hello World 2!');
