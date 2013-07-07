ec5f654231e73eedf1af0cf3b675d7705665c2c5
aakritiagarwal <agarwal.aakriti@gmail.com>
2013-07-07 16:21:24 +0000

    errors fixed

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //var buffer = new Buffer(28);
   var buffer  = fs.readFileSync('index.html');
-  response.send(buffer.toString("utf-8");
+  response.send(buffer.toString("utf-8"));
   //response.send('Hello World 2!');
-  console.log(buffer.toString("utf-8");
+//  console.log(buffer.toString("utf-8");
 });
 
 var port = process.env.PORT || 5000;
